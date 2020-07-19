5cf845b312235d1cdf7b39a157cf92c7b817a720
rariruren555 <67192257+rariruren555@users.noreply.github.com>
2020-07-19 15:27:06 +0900

    Update Search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:edit, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :search]
 
   def index
     @posts = Post.includes(:user).order("created_at DESC")
@@ -38,6 +38,9 @@ class PostsController < ApplicationController
     @comments = @post.comments.includes(:user)
   end
 
+  def search
+    @posts = Post.search(params[:keyword])
+  end
 
   private
   def post_params
