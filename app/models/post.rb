4092f9d3c4e7a1e386e607e7b5d1a2a5b8208711
rariruren555 <67192257+rariruren555@users.noreply.github.com>
2020-08-18 23:01:47 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
 
   belongs_to :user
   has_many :comments
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
   def self.search(search)
