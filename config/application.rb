774cdfdc17aa7d85299a8fd0afa7cb20ad069fa5
rariruren555 <67192257+rariruren555@users.noreply.github.com>
2020-08-19 18:04:17 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module GameFavorite
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
+    config.assets.initialize_on_precompile = false
   end
-  config.assets.initialize_on_precompile = false
 end
